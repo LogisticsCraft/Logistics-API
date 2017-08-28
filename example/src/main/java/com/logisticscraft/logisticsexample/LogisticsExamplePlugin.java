86139f6c42753f3c5e6fa3345b0d4a4e28742f17
Gabriele C <sgdc3.mail@gmail.com>
2017-08-28 13:17:59 +0200

    Prepare modules

---
+++ example/src/main/java/com/logisticscraft/logisticsexample/LogisticsExamplePlugin.java
@@ -0,0 +1,22 @@
+package com.logisticscraft.logisticsexample;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author JARvis (Пётр) PROgrammer
+ */
+public final class LogisticsExamplePlugin extends JavaPlugin {
+    private static LogisticsExamplePlugin instance;
+
+    public static LogisticsExamplePlugin getInstance() {
+        return instance;
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    @Override
+    public void onEnable() {
+    }
+}

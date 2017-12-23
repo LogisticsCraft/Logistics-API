a740dba105bab136f809cc0f1a351931042bcc16
sgdc3 <sgdc3.mail@gmail.com>
2017-12-23 17:26:08 +0100

    Whoops

--- example/src/main/java/com/logisticscraft/logisticsexample/blocks/TestBlock.java
+++ example/src/main/java/com/logisticscraft/logisticsexample/blocks/TestBlock.java
@@ -1,6 +1,8 @@
 package com.logisticscraft.logisticsexample.blocks;
 
 import com.logisticscraft.logisticsapi.block.LogisticBlock;
+import com.logisticscraft.logisticsapi.data.LogisticKey;
+import com.logisticscraft.logisticsapi.data.SafeBlockLocation;
 import com.logisticscraft.logisticsapi.energy.EnergyInput;
 import com.logisticscraft.logisticsapi.energy.EnergyOutput;
 import com.logisticscraft.logisticsapi.energy.EnergyStorage;
@@ -10,4 +12,7 @@ import com.logisticscraft.logisticsapi.energy.EnergyStorage;
 @EnergyOutput.EnergyOutputData(maxExtract = 100)
 public class TestBlock extends LogisticBlock implements EnergyInput, EnergyOutput {
 
+    public TestBlock(LogisticKey typeId, String name, SafeBlockLocation location) {
+        super(typeId, name, location);
+    }
 }

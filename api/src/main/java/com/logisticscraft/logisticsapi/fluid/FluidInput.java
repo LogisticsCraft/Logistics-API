31b4c5ef83d4cb1aa30f2aa1eb95a0cdd9e230fa
tr7zw <tr7zw@timolia.de>
2017-12-24 02:06:13 +0100

    Optional

--- api/src/main/java/com/logisticscraft/logisticsapi/fluid/FluidInput.java
+++ api/src/main/java/com/logisticscraft/logisticsapi/fluid/FluidInput.java
@@ -1,21 +1,20 @@
 package com.logisticscraft.logisticsapi.fluid;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Optional;
+
 import com.logisticscraft.logisticsapi.data.LogisticBlockFace;
 import com.logisticscraft.logisticsapi.utils.ReflectionUtils;
 
 import lombok.NonNull;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-import org.jetbrains.annotations.Nullable;
-
 public interface FluidInput extends FluidStorage {
 
     default long receiveFluid(@NonNull LogisticBlockFace blockFace, @NonNull LogisticFluid fluid, final long available, final boolean simulate) {
-        if(!allowFluidInput(blockFace, fluid))return 0;
+        if(!allowFluidInput(blockFace, Optional.of(fluid)))return 0;
     	long amountReceived = Math.min(getMaxFluidStored() - getStoredFluidAmount(), Math.min(getMaxReceive(), available));
         if (!simulate) {
             setStoredFluidAmount(getStoredFluidAmount() + amountReceived);
@@ -27,10 +26,7 @@ public interface FluidInput extends FluidStorage {
         return ReflectionUtils.getClassAnnotation(this, FluidInputData.class).maxReceive();
     }
     
-    /*
-     * Null fluid is allowed for pipes to check for connecting to blockfaces
-    */
-    default boolean allowFluidInput(@NonNull LogisticBlockFace blockFace, @Nullable LogisticFluid fluid){
+    default boolean allowFluidInput(@NonNull LogisticBlockFace blockFace, Optional<LogisticFluid> fluid){
     	return true;
     }
 

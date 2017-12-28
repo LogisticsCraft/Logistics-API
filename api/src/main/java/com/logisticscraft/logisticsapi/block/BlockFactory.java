631602c6f0d4c3d19213c4b4c03f76e34a7620d2
sgdc3 <sgdc3.mail@gmail.com>
2017-12-28 23:56:58 +0100

    Exclude excluded packages from javadocs

--- api/src/main/java/com/logisticscraft/logisticsapi/block/BlockFactory.java
+++ api/src/main/java/com/logisticscraft/logisticsapi/block/BlockFactory.java
@@ -8,8 +8,8 @@ import org.bukkit.inventory.ItemStack;
 
 public interface BlockFactory {
 
-    public LogisticBlock onPlace(@NonNull Player player, @NonNull ItemStack item, @NonNull Location location);
+    LogisticBlock onPlace(@NonNull Player player, @NonNull ItemStack item, @NonNull Location location);
 
-    public LogisticBlock onLoad(@NonNull NBTCompound nbtData);
+    LogisticBlock onLoad(@NonNull NBTCompound nbtData);
 
 }
